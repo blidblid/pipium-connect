c96fa5e24786b6926bb66eea5d58dc5b5dd1a058
Albin Berglund <e.a.e.berglund@gmail.com>
2024-11-01 18:56:06 +0100

    bump

--- python-connect/setup.py
+++ python-connect/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="pipium_connect",
-    version="0.0.1a4",  # Also change in pyproject.toml
+    version="0.0.1a5",  # Also change in pyproject.toml
     packages=find_packages(),
 )
